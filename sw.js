58fedf3ea1a6ea9198cb1f168f37330fdcfc4f1d
Yannic Luyckx <yannic.luyckx@gmail.com>
2021-12-25 15:31:10 -0500

    correction de path

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/',
       '/pwa/index.html',
       '/pwa/index.js',
       '/pwa/style.css',
