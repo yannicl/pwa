4fd9df86e340c454aa1fa49424c225141d805df6
Yannic Luyckx <yannic.luyckx@gmail.com>
2021-12-25 15:26:07 -0500

    correction de path

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/pwa-examples/a2hs/sw.js')
+    .register('/pwa/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
